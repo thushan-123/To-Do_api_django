a6de105814009250f154d9f9a3368ed021a28f5b
thushan-123 <thushanmadusanka456@gmail.com>
2026-09-13 19:02:01 +0530

    create user reg fn

--- Users/views.py
+++ Users/views.py
@@ -5,3 +5,14 @@ from rest_framework.decorators import api_view
 from models import User
 from serializers import UserSerializer
 
+@api_view(['POST'])
+def registerUser(request):
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+
