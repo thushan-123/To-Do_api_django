c63dedf830d8faceb58e9128ae27d68231f45e7a
thushan-123 <thushanmadusanka456@gmail.com>
2026-09-13 18:58:21 +0530

    imports nesssary libs

---
+++ Users/views.py
@@ -0,0 +1,7 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from models import User
+from serializers import UserSerializer
+
