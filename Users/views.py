8aecce4a2592acc20ae62188644dc1c6cd9e89c7
thushan-123 <thushanmadusanka456@gmail.com>
2026-09-13 19:12:55 +0530

    imple fn delete user

--- Users/views.py
+++ Users/views.py
@@ -34,3 +34,13 @@ def updateUser(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
+@api_view(['DELETE'])
+def deleteUser(request):
+    try:
+        if request.method == 'DELETE':
+            user = User.objects.get(id=request.data['id'])
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
