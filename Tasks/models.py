86a7ad3bd3ba662ef35816b1404d86b69e45b3a7
thushan-123 <thushanmadusanka456@gmail.com>
2026-09-14 15:32:13 +0530

    fix some bugs

--- Tasks/models.py
+++ Tasks/models.py
@@ -1,10 +1,16 @@
-import uuid
 
 from django.db import models
 
+from Users.models import User
+
+
 class Task(models.Model):
     task_id = models.AutoField(primary_key=True)
-    user_id = models.AutoField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
     title = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateField()
--- Tasks/views.py
+++ Tasks/views.py
@@ -2,14 +2,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from models import Task
-from serializers import TaskSerializer
+from .models import Task
+from .serializers import TaskSerializer
 
 @api_view(['GET'])
 def filter_tasks(request, user_id):
     try:
         if request.method == 'GET':
-            tasks = Task.objects.get(user_id=user_id)
+            tasks = Task.objects.filter(user_id=user_id)
             serializer = TaskSerializer(tasks, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_404_NOT_FOUND)
@@ -28,18 +28,32 @@ def create_task(request):
 
 @api_view(['PUT'])
 def update_task(request, task_id):
-    if request.method == 'PUT':
-        serializer = TaskSerializer(instance=request.data, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return Response(status=status.HTTP_401_UNAUTHORIZED)
+    try:
+        task = Task.objects.get(task_id=task_id)
+    except Task.DoesNotExist:
+        return Response(
+            {"error": "Task not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    serializer = TaskSerializer(task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST
+    )
 
 @api_view(['DELETE'])
 def delete_task(request, task_id):
     if request.method == 'DELETE':
-        Task.objects.filter(id=task_id).delete()
+        Task.objects.filter(task_id=task_id).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
